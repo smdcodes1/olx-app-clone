aa48198fbcc0486da53970e8264aef19bc172ce8
smdcodes1 <ace567871@gmail.com>
2025-11-29 15:27:22 +0530

    Tenth updation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 from django.urls import path,include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from django.http import JsonResponse
 
+def health(request):
+    return JsonResponse({"status": "Backend is running"})
 urlpatterns = [
+    path("", health),
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
